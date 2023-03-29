48c5f8931baa8775cbf0e13b6f7a7ce351205727
Tomokou <bychckova.margo@yandex.ru>
2023-03-29 14:14:17 +0300

    Initial commit

---
+++ src/main/java/com/example/sockstoreapp/SockStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockStoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sockstoreapp/SockStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
